7bcc6b08c337b416f55245fe7cafd2cf6f69bc86
ayushkr4383 <ayushkr4383@gmail.com>
2026-09-02 23:30:00 +0530

    Daily practice

---
+++ 0075-Sort_colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int i,j,temp;
+        for(i=0;i<n-1;i++){
+            for(j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+ 
+    }
+}
